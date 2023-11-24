75a70385366f621a5a66821a8c546feb38a0ea3a
FOUAD SAIDI <107761768+fouad-saidi-dev@users.noreply.github.com>
2023-11-24 12:57:14 +0100

    v1.0

--- src/main/java/com/blog/controllers/PostController.java
+++ src/main/java/com/blog/controllers/PostController.java
@@ -9,17 +9,21 @@ import org.springframework.beans.BeanUtils;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 import java.security.Principal;
 
 @RestController
+@RequestMapping("/post")
 public class PostController {
 
     @Autowired
     PostService postService;
 
+    @PostMapping("/create")
     public ResponseEntity<PostResponse> createPost(@RequestBody PostRequest postRequest, Principal principal) {
 
         PostDto postDto = new PostDto();
