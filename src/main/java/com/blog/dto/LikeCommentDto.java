922a0b65546b611eb41ceb181833ca0b2e650658
FOUAD SAIDI <107761768+fouad-saidi-dev@users.noreply.github.com>
2023-12-03 23:42:33 +0100

    v1.9

--- src/main/java/com/blog/dto/LikeCommentDto.java
+++ src/main/java/com/blog/dto/LikeCommentDto.java
@@ -1,4 +1,21 @@
 package com.blog.dto;
 
+import com.blog.entities.Comment;
+import com.blog.entities.User;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import java.time.LocalDateTime;
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
 public class LikeCommentDto {
+    private Long Id;
+    private String LikeCommentId;
+    private Boolean isLike;
+    private LocalDateTime createdAt;
+    private LocalDateTime updatedAt;
+    private User user;
+    private Comment comment;
 }
